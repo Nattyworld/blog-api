96a325acd8315e76e7abf783d7e0d42185be2927
Nathaniel Oluji <olujinathaniel332@gmail.com>
2024-12-28 16:12:28 +0100

    bcrypt error fix

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,6 +1,6 @@
 const User = require('../models/usermodels');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
   try {
